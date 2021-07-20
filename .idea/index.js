c94b278dae558b1e9f9d28eb5caa0e16b82b2204
Fabrizio <a20160348@pucp.edu.pe>
2021-07-20 13:08:21 -0500

    chat 2

--- .idea/index.js
+++ .idea/index.js
@@ -10,10 +10,9 @@ var server = http.Server(app);
 var io = socketIO(server);
 var conexiones = 0;
 var hash = sha256.create();
-const multer  = require('multer')
+const multer = require('multer')
 let upload = multer();
 let crypto = require('crypto');
-
 
 
 let conn = mysql.createConnection({
@@ -22,17 +21,17 @@ let conn = mysql.createConnection({
     password: 'root',
     database: 'exfinal_g7',
     port: 3306,
-    typeCast: function (field,next){
-        if(field.type === 'BIT'){
-            return (field.buffer()[0]===1);
-        } else{
+    typeCast: function (field, next) {
+        if (field.type === 'BIT') {
+            return (field.buffer()[0] === 1);
+        } else {
             return next();
         }
     }
 });
 
-conn.connect(function(err){
-    if(err) throw err;
+conn.connect(function (err) {
+    if (err) throw err;
     console.log("Conexión exitosa a base de datos");
 });
 
@@ -40,22 +39,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post('/acceder', upload.none(), function (req, res) {
     let usuario = req.body.usuario;
     let contra = req.body.contra;
-    var hashpassword=crypto.createHash("sha256");
-    hashpassword.update (contra);
-    var sha256c = hashpassword.digest ("hex"); // mostrado como hexadecimal
-    console.log (sha256c);
+    var hashpassword = crypto.createHash("sha256");
+    hashpassword.update(contra);
+    var sha256c = hashpassword.digest("hex"); // mostrado como hexadecimal
+    console.log(sha256c);
     var params = [usuario, sha256c];
     if (usuario && contra) {
         var sql = "select * from usuario where nombre =? and contra =?";
-        conn.query(sql, params, function(error, results) {
+        conn.query(sql, params, function (error, results) {
             console.log(results);
             if (error) {
                 res.redirect('/login');
-            } if (results[0]==null){
+            }
+            if (results[0] == null) {
                 res.send('¡Usuario y/o Contraseña no válidos!');
                 console.log(error);
-            }
-            else {
+            } else {
                 console.log("exito");
                 //req.session.loggedin = true;
                 // req.session.username = username;
@@ -68,10 +67,10 @@ app.post('/acceder', upload.none(), function (req, res) {
         res.end();
     }
 });
-app.get('/login',function (req, res) {
+app.get('/login', function (req, res) {
     res.sendFile(__dirname + '/login.html');
 });
-app.get('/chat',function (req, res) {
+app.get('/chat', function (req, res) {
     res.sendFile(__dirname + '/chat.html');
 });
 
@@ -79,13 +78,15 @@ app.listen(3000, function () {
     console.log("Servidor corriendo en el puerto 3000");
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log("Usuario conectado");
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log("Usuario desconectado");
     })
 
-    socket.on('chat message')
-
+    socket.on('chat message', function (msg) {
+        console.log("Mensaje recibido: " + msg);
     })
+
+});
